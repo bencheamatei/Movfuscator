a361a19a8212a385670e2636853ccd83e6e7fd87
Matei Benchea <108959258+bencheamatei@users.noreply.github.com>
2025-12-31 19:27:13 +0200

    Add files via upload

---
+++ lookup_generator.py
@@ -0,0 +1,104 @@
+def genxor():
+    a=bytearray()
+    for x in range(256):
+        for y in range(256):
+            a.append((x^y)&0xFF)
+
+    with open("xor.bin","wb") as fout:
+        fout.write(a)
+
+def genand():
+    a=bytearray()
+    for x in range(256):
+        for y in range(256):
+            a.append((x&y)&0xFF)
+
+    with open("and.bin","wb") as fout:
+        fout.write(a)
+
+def genor():
+    a=bytearray()
+    for x in range(256):
+        for y in range(256):
+            a.append((x|y)&0xFF)
+
+    with open("or.bin","wb") as fout:
+        fout.write(a)
+
+def gennot():
+    a=bytearray()
+    for x in range(256):
+        a.append((~x)&0xFF)
+
+    with open("not.bin","wb") as fout:
+        fout.write(a)
+
+def geninc():
+    a=bytearray()
+    for x in range(256):
+        a.append((x+1)&0xFF)
+    with open("inc.bin","wb") as fout:
+        fout.write(a)
+def gendec():
+    a=bytearray()
+    for x in range(256):
+        a.append((x-1)&0xFF)
+    with open("dec.bin","wb") as fout:
+        fout.write(a)
+def genadd():
+    a=bytearray()
+    b=bytearray()
+    for x in range(256):
+        for y in range(256):
+            a.append((x+y)&0xFF)
+            if x+y>255:
+                b.append(1)
+            else:
+                b.append(0)
+
+    for x in range(256):
+        for y in range(256):
+            a.append((x+y+1)&0xFF)
+            if x+y+1>255:
+                b.append(1)
+            else:
+                b.append(0)
+    with open("add.bin","wb") as fout:
+        fout.write(a)
+    with open("carry.bin","wb") as fout:
+        fout.write(b)
+
+def gensub():
+    a=bytearray()
+    b=bytearray()
+    for x in range(256):
+        for y in range(256):
+            a.append((x-y)&0xFF)
+            if x-y<0:
+                b.append(1)
+            else:
+                b.append(0)
+
+
+    for x in range(256):
+        for y in range(256):
+            a.append((x-y-1)&0xFF)
+            if x-y-1<0:
+                b.append(1)
+            else:
+                b.append(0)
+
+
+    with open("sub.bin","wb") as fout:
+        fout.write(a)
+    with open("carry_sub.bin","wb") as fout:
+        fout.write(b)
+
+genand()
+gennot()
+genor()
+genxor()
+geninc()
+gendec()
+genadd()
+gensub()
