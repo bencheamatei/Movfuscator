aa5934310e6a1b24385f842dc30f307e27ca7287
Matei Benchea <108959258+bencheamatei@users.noreply.github.com>
2025-12-31 15:08:08 +0200

    Update main_mov.s

--- main_mov.s
+++ main_mov.s
@@ -3,6 +3,7 @@ and_table: .incbin "and.bin"
 or_table: .incbin "or.bin"
 xor_table: .incbin "xor.bin"
 not_table: .incbin "not.bin"
+inc_table: .incbin "inc.bin"
 save_eax: .space 4
 save_ebx: .space 4
 save_ecx: .space 4
@@ -13,16 +14,15 @@ tmp_src: .space 4
 tmp_dest: .space 4
 tmp_ans: .space 4
 fs: .asciz "%ld\n"
+n: .long 10
 .text
 .global main
 main:
-movl $8, %eax
-movl $1, %ebx
 movl %eax, save_eax
 movl %ecx, save_ecx
 movl %edi, save_edi
-movl %eax, tmp_src
-movl %ebx, tmp_dest
+movl %ecx, tmp_src
+movl %ecx, tmp_dest
 movl $0, tmp_ans
 movl $xor_table, %edi
 movl $0, %ecx
@@ -45,36 +45,37 @@ movb %al, tmp_ans+3
 movl save_edi, %edi
 movl save_ecx, %ecx
 movl save_eax, %eax
-movl tmp_ans, %ebx
-
-movl %eax, save_eax
-movl %ecx, save_ecx
+movl tmp_ans, %ecx
 movl %edi, save_edi
-movl %ebx, tmp_dest
-movl $0, tmp_ans
-movl $not_table, %edi
-movl $0, %ecx
-movb tmp_dest+0, %cl
-movb (%edi, %ecx), %al
-movb %al, tmp_ans+0
-movb tmp_dest+1, %cl
-movb (%edi, %ecx), %al
-movb %al, tmp_ans+1
-movb tmp_dest+2, %cl
-movb (%edi, %ecx), %al
-movb %al, tmp_ans+2
-movb tmp_dest+3, %cl
-movb (%edi, %ecx), %al
-movb %al, tmp_ans+3
-movl save_edi, %edi
-movl save_ecx, %ecx
+movl %eax, save_eax
+movl $inc_table, %edi
+movl %ecx, temp_dest
+movl $0, temp_ans
+movl $0, %eax
+movb temp_dest+0, %al
+movb (%edi, %eax), %al
+movb %al, temp_ans+0
+cmpb $0, %al
+jne fin_inc
+movb temp_dest+1, %al
+movb (%edi, %eax), %al
+movb %al, temp_ans+1
+cmpb $0, %al
+jne fin_inc
+movb temp_dest+2, %al
+movb (%edi, %eax), %al
+movb %al, temp_ans+2
+cmpb $0, %al
+jne fin_inc
+movb temp_dest+3, %al
+movb (%edi, %eax), %al
+movb %al, temp_ans+3
+cmpb $0, %al
+jne fin_inc
+fin_inc:
+movl temp_ans, %ecx
 movl save_eax, %eax
-movl tmp_ans, %ebx
-
-pushl %ebx
-pushl $fs
-call printf
-addl $8, %esp
+movl save_edi, %edi
 
 movl $1, %eax
 movl %eax, save_eax
