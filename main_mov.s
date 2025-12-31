ae00ef57144b0286bbd8a9b85c04175d3a8896b8
Matei Benchea <108959258+bencheamatei@users.noreply.github.com>
2025-12-31 17:21:03 +0200

    haz

--- main_mov.s
+++ main_mov.s
@@ -19,21 +19,109 @@ save_edi: .space 4
 tmp_src: .space 4
 tmp_dest: .space 4
 tmp_ans: .space 4
-fs: .asciz "%ld\n"
-n: .long 10
+n: .space 4
+k: .space 4
+fstring: .asciz "%ld\n"
+fsc: .asciz "%ld %ld"
 .text
-.global main
-main:
-movl $5, %eax
-movl $4, %ebx
+
+comb:
+pushl %ebp
+movl %esp, %ebp
+
+movl 8(%ebp), %ecx
+movl 12(%ebp), %edx
+
+cmp $0, %ecx
+je base_case
+
+cmp %ecx, %edx
+je base_case
+
+cmp $0, %edx
+je base_case
+
+movl %edi, save_edi
+movl %eax, save_eax
+movl $dec_table, %edi
+movl %ecx, tmp_dest
+movl $0, tmp_ans
+movl $0, %eax
+movb tmp_dest+0, %al
+movb (%edi, %eax), %al
+movb %al, tmp_ans+0
+cmpb $255, %al
+jne fin_dec0
+movb tmp_dest+1, %al
+movb (%edi, %eax), %al
+movb %al, tmp_ans+1
+cmpb $255, %al
+jne fin_dec0
+movb tmp_dest+2, %al
+movb (%edi, %eax), %al
+movb %al, tmp_ans+2
+cmpb $255, %al
+jne fin_dec0
+movb tmp_dest+3, %al
+movb (%edi, %eax), %al
+movb %al, tmp_ans+3
+cmpb $255, %al
+jne fin_dec0
+fin_dec0:
+movl tmp_ans, %ecx
+movl save_eax, %eax
+movl save_edi, %edi
+pushl %edx
+pushl %ecx
+call comb
+popl %ecx
+popl %edx
+
+pushl %eax
+movl %edi, save_edi
+movl %eax, save_eax
+movl $dec_table, %edi
+movl %edx, tmp_dest
+movl $0, tmp_ans
+movl $0, %eax
+movb tmp_dest+0, %al
+movb (%edi, %eax), %al
+movb %al, tmp_ans+0
+cmpb $255, %al
+jne fin_dec1
+movb tmp_dest+1, %al
+movb (%edi, %eax), %al
+movb %al, tmp_ans+1
+cmpb $255, %al
+jne fin_dec1
+movb tmp_dest+2, %al
+movb (%edi, %eax), %al
+movb %al, tmp_ans+2
+cmpb $255, %al
+jne fin_dec1
+movb tmp_dest+3, %al
+movb (%edi, %eax), %al
+movb %al, tmp_ans+3
+cmpb $255, %al
+jne fin_dec1
+fin_dec1:
+movl tmp_ans, %edx
+movl save_eax, %eax
+movl save_edi, %edi
+pushl %edx
+pushl %ecx
+call comb
+popl %ecx
+popl %edx
+
 movl %eax, save_eax
 movl %ebx, save_ebx
 movl %ecx, save_ecx
 movl %edi, save_edi
 movl $0, carryval
 movl $0, tmp_ans
-movl %eax, tmp_src
-movl %ebx, tmp_dest
+movl 0(%esp), tmp_src
+movl %eax, tmp_dest
 movl carryval, %ebx
 movl addp(, %ebx, 4), %edi
 movl $0, %ecx
@@ -78,12 +166,173 @@ movl %eax, save_eax
 movl %ebx, save_ebx
 movl %ecx, save_ecx
 movl %edi, save_edi
-movl tmp_ans, %ebx
+movl tmp_ans, %eax
+jmp combr
 
-pushl %ebx
-pushl $fs
+bad_case:
+movl %eax, save_eax
+movl %ecx, save_ecx
+movl %edi, save_edi
+movl %eax, tmp_src
+movl %eax, tmp_dest
+movl $0, tmp_ans
+movl $xor_table, %edi
+movl $0, %ecx
+movb tmp_src+0, %ch
+movb tmp_dest+0, %cl
+movb (%edi, %ecx), %al
+movb %al, tmp_ans+0
+movb tmp_src+1, %ch
+movb tmp_dest+1, %cl
+movb (%edi, %ecx), %al
+movb %al, tmp_ans+1
+movb tmp_src+2, %ch
+movb tmp_dest+2, %cl
+movb (%edi, %ecx), %al
+movb %al, tmp_ans+2
+movb tmp_src+3, %ch
+movb tmp_dest+3, %cl
+movb (%edi, %ecx), %al
+movb %al, tmp_ans+3
+movl save_edi, %edi
+movl save_ecx, %ecx
+movl save_eax, %eax
+movl tmp_ans, %eax
+jmp combr
+base_case:
+movl $1, %eax
+jmp combr
+combr:
+movl %ebp, %esp
+popl %ebp
+ret
+.global main
+main:
+pushl $k
+pushl $n
+pushl $fsc
+call scanf
+popl %eax
+popl %eax
+
+pushl k
+pushl n
+call comb
+movl %eax, save_eax
+movl %ebx, save_ebx
+movl %ecx, save_ecx
+movl %edi, save_edi
+movl $0, carryval
+movl $0, tmp_ans
+movl $4, tmp_src
+movl %esp, tmp_dest
+movl carryval, %ebx
+movl addp(, %ebx, 4), %edi
+movl $0, %ecx
+movb tmp_src+0, %cl
+movb tmp_dest+0, %ch
+movb (%edi, %ecx), %al
+movb %al, tmp_ans+0
+movl carryp(, %ebx, 4), %edi
+movb (%edi, %ecx), %al
+movb %al, carryval+0
+movl carryval, %ebx
+movl addp(, %ebx, 4), %edi
+movl $0, %ecx
+movb tmp_src+1, %cl
+movb tmp_dest+1, %ch
+movb (%edi, %ecx), %al
+movb %al, tmp_ans+1
+movl carryp(, %ebx, 4), %edi
+movb (%edi, %ecx), %al
+movb %al, carryval+0
+movl carryval, %ebx
+movl addp(, %ebx, 4), %edi
+movl $0, %ecx
+movb tmp_src+2, %cl
+movb tmp_dest+2, %ch
+movb (%edi, %ecx), %al
+movb %al, tmp_ans+2
+movl carryp(, %ebx, 4), %edi
+movb (%edi, %ecx), %al
+movb %al, carryval+0
+movl carryval, %ebx
+movl addp(, %ebx, 4), %edi
+movl $0, %ecx
+movb tmp_src+3, %cl
+movb tmp_dest+3, %ch
+movb (%edi, %ecx), %al
+movb %al, tmp_ans+3
+movl carryp(, %ebx, 4), %edi
+movb (%edi, %ecx), %al
+movb %al, carryval+0
+movl %eax, save_eax
+movl %ebx, save_ebx
+movl %ecx, save_ecx
+movl %edi, save_edi
+movl tmp_ans, %esp
+movl %eax, save_eax
+movl %ebx, save_ebx
+movl %ecx, save_ecx
+movl %edi, save_edi
+movl $0, carryval
+movl $0, tmp_ans
+movl $4, tmp_src
+movl %esp, tmp_dest
+movl carryval, %ebx
+movl addp(, %ebx, 4), %edi
+movl $0, %ecx
+movb tmp_src+0, %cl
+movb tmp_dest+0, %ch
+movb (%edi, %ecx), %al
+movb %al, tmp_ans+0
+movl carryp(, %ebx, 4), %edi
+movb (%edi, %ecx), %al
+movb %al, carryval+0
+movl carryval, %ebx
+movl addp(, %ebx, 4), %edi
+movl $0, %ecx
+movb tmp_src+1, %cl
+movb tmp_dest+1, %ch
+movb (%edi, %ecx), %al
+movb %al, tmp_ans+1
+movl carryp(, %ebx, 4), %edi
+movb (%edi, %ecx), %al
+movb %al, carryval+0
+movl carryval, %ebx
+movl addp(, %ebx, 4), %edi
+movl $0, %ecx
+movb tmp_src+2, %cl
+movb tmp_dest+2, %ch
+movb (%edi, %ecx), %al
+movb %al, tmp_ans+2
+movl carryp(, %ebx, 4), %edi
+movb (%edi, %ecx), %al
+movb %al, carryval+0
+movl carryval, %ebx
+movl addp(, %ebx, 4), %edi
+movl $0, %ecx
+movb tmp_src+3, %cl
+movb tmp_dest+3, %ch
+movb (%edi, %ecx), %al
+movb %al, tmp_ans+3
+movl carryp(, %ebx, 4), %edi
+movb (%edi, %ecx), %al
+movb %al, carryval+0
+movl %eax, save_eax
+movl %ebx, save_ebx
+movl %ecx, save_ecx
+movl %edi, save_edi
+movl tmp_ans, %esp
+
+pushl %eax
+pushl $fstring
 call printf
 popl %eax
+popl %eax
+
+pushl $0
+call fflush
 popl %eax
 
 movl $1, %eax
